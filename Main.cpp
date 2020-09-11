27e7d25fbbee6231e43274f80c60a852d77543c9
Sergii Shagan <siroga.shagan@gmail.com>
2020-09-11 16:52:37 +0300

    Fixed bug

--- Main.cpp
+++ Main.cpp
@@ -9,12 +9,15 @@ using namespace sf;
 
 #include "Map.h"
 
+int N = 8;
+
 int Kolvo_In_Osndia_FOXs(int radok, int stovbec, int * Masiv, int N);
 int Kolvo_In_Pobdia_FOXs(int radok, int stovbec, int * Masiv, int N);
 int Kolvo_In_Radok_FOXs(int radok, int stovbec, int * Masiv, int N);
 int Kolvo_In_Stovbets_FOXs(int radok, int stovbec, int * Masiv, int N);
 void Welcome();
 void You_Win();
+void pull_mas(int *Masiv);
 int Menu(RenderWindow &window);
 
 int main()
@@ -26,7 +29,6 @@ int main()
 	{
 		int radok, stovbec;
 		srand(time(NULL));
-		int N = 8;
 		int Fox = N;
 		int Hod = 0;
 		int Kolvo_pobdia, Kolvo_osndia, Kolvo_Radok, Kolvo_stovbets;
@@ -34,32 +36,7 @@ int main()
 		int *Masiv;
 		Masiv = (int *)malloc(N * N * sizeof(int));
 
-
-		for (int i = 0; i < N; i++)
-		{
-			for (int j = 0; j < N; j++)
-			{
-				Masiv[i * N + j] = 0;
-			}
-		}
-
-		for (int i = 0; i < N; i++)
-		{
-			radok = 0 + rand() % 8;
-			stovbec = 0 + rand() % 8;
-			if (Masiv[radok * N + stovbec] == 1)
-			{
-				i--;
-			}
-			else
-			{
-				Masiv[radok * N + stovbec] = 1;
-			}
-			
-			
-		}
-
-
+		pull_mas(Masiv);
 
 		Image HunterImage;
 		HunterImage.loadFromFile("pictures/Hunter.png");
@@ -103,19 +80,19 @@ int main()
 
 		text.setStyle(sf::Text::Bold | sf::Text::Underlined);
 
+		Event event;
+
 		while (window.isOpen())
 		{
 			float time = clock.getElapsedTime().asMicroseconds();
 			clock.restart();
 			time = time / 800;
-			Event event;
-			while (window.pollEvent(event))
+
+			if (Keyboard::isKeyPressed(Keyboard::Escape))
 			{
-				if (Keyboard::isKeyPressed(Keyboard::Escape))
-				{
-					k = Menu(window);
-				}
+				k = Menu(window);
 			}
+
 			window.clear();
 
 			if ((H >= BeforeH) && (V > BeforeV))
@@ -218,9 +195,8 @@ int main()
 			}
 
 
-
-
 			for (int i = 0; i < HEIGHT_MAP; i++)
+			{
 				for (int j = 0; j < WIDTH_MAP; j++)
 				{
 					if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 100, 50)); 
@@ -229,12 +205,18 @@ int main()
 					if ((TileMap[i][j] == ' ') && (IntRect(j * 100, i * 50, 100, 50).contains(Mouse::getPosition(window))))
 					{ 
 						s_map.setColor(Color::Blue);
-						if(Mouse::isButtonPressed(Mouse::Left))
+
+						while(window.pollEvent(event))
 						{
-							V = 25 + (i - 1) * 50;
-							H = 125 + (j - 1) * 100;
-							Hod++;
-							Fox--;
+							if (event.type == Event::MouseButtonReleased)
+							{
+								if (event.mouseButton.button == sf::Mouse::Left)
+								{
+									V = 25 + (i - 1) * 50;
+									H = 125 + (j - 1) * 100;
+									Hod++;
+								}
+							}	
 						}
 					}
 
@@ -244,6 +226,13 @@ int main()
 					window.draw(s_map);
 					s_map.setColor(Color::White);
 				}
+			}
+
+			while(window.pollEvent(event))
+			{
+				//clear event buffer!
+			}
+			
 
 			window.draw(HunterSprite);
 			Kolvo_osndia = Kolvo_In_Osndia_FOXs(radok, stovbec, Masiv, N);
@@ -251,47 +240,47 @@ int main()
 			Kolvo_Radok = Kolvo_In_Radok_FOXs(radok, stovbec, Masiv, N);
 			Kolvo_stovbets = Kolvo_In_Stovbets_FOXs(radok, stovbec, Masiv, N);
 
-			text.setPosition(1000, 25);
+			text.setPosition(900, 25);
 
 			std::ostringstream playerScoreString1;
 			playerScoreString1 << Kolvo_osndia;
-			text.setString("Fox in Osndia: " + playerScoreString1.str());
+			text.setString("Fox in Main Diagonal: " + playerScoreString1.str());
 
 			window.draw(text);
 
-			text.setPosition(1000, 50);
+			text.setPosition(900, 50);
 
 			std::ostringstream playerScoreString2;
 			playerScoreString2 << Kolvo_pobdia;
-			text.setString("Fox in Pobdia: " + playerScoreString2.str());
+			text.setString("Fox in Side Diagonal: " + playerScoreString2.str());
 
 			window.draw(text);
 
-			text.setPosition(1000, 75);
+			text.setPosition(900, 75);
 
 			std::ostringstream playerScoreString3;
 			playerScoreString3 << Kolvo_Radok;
-			text.setString("Fox in Radok: " + playerScoreString3.str());
+			text.setString("Fox in Row: " + playerScoreString3.str());
 
 			window.draw(text);
 
-			text.setPosition(1000, 100);
+			text.setPosition(900, 100);
 
 			std::ostringstream playerScoreString4;
 			playerScoreString4 << Kolvo_stovbets;
-			text.setString("Fox in Stovbets: " + playerScoreString4.str());
+			text.setString("Fox in Column: " + playerScoreString4.str());
 
 			window.draw(text);
 
-			text.setPosition(1000, 200);
+			text.setPosition(900, 200);
 
 			std::ostringstream playerScoreString5;
 			playerScoreString5 << Fox;
-			text.setString("Fox: " + playerScoreString5.str());
+			text.setString("You should to search " + playerScoreString5.str() + " foxes");
 
 			window.draw(text);
 
-			text.setPosition(1000, 250);
+			text.setPosition(900, 250);
 
 			std::ostringstream playerScoreString6;
 			playerScoreString6 << Hod;
@@ -299,42 +288,44 @@ int main()
 
 			window.draw(text);
 
-			playerScoreString6.clear();
-			playerScoreString6.str("");
-			text.setPosition(1000, 300);
-			playerScoreString6 << RealH;
-			text.setString("RealH: " + playerScoreString6.str());
-			window.draw(text);
+			// playerScoreString6.clear();
+			// playerScoreString6.str("");
+			// text.setPosition(1000, 300);
+			// playerScoreString6 << RealH;
+			// text.setString("RealH: " + playerScoreString6.str());
+			// window.draw(text);
 			
-			playerScoreString6.clear();
-			playerScoreString6.str("");
-			text.setPosition(1000, 350);
-			playerScoreString6 << RealV;
-			text.setString("RealV: " + playerScoreString6.str());
-			window.draw(text);
+			// playerScoreString6.clear();
+			// playerScoreString6.str("");
+			// text.setPosition(1000, 350);
+			// playerScoreString6 << RealV;
+			// text.setString("RealV: " + playerScoreString6.str());
+			// window.draw(text);
 
 
-			playerScoreString6.clear();
-			playerScoreString6.str("");
-			text.setPosition(1000, 400);
-			playerScoreString6 << H;
-			text.setString("H: " + playerScoreString6.str());
+			// playerScoreString6.clear();
+			// playerScoreString6.str("");
+			// text.setPosition(1000, 400);
+			// playerScoreString6 << H;
+			// text.setString("H: " + playerScoreString6.str());
 
-			window.draw(text);
+			// window.draw(text);
 			
-			playerScoreString6.clear();
-			playerScoreString6.str("");
-			text.setPosition(1000, 450);
-			playerScoreString6 << V;
-			text.setString("V: " + playerScoreString6.str());
-			window.draw(text);
+			// playerScoreString6.clear();
+			// playerScoreString6.str("");
+			// text.setPosition(1000, 450);
+			// playerScoreString6 << V;
+			// text.setString("V: " + playerScoreString6.str());
+			// window.draw(text);
 
 			window.display();
-
 			if (Fox == 0)
 			{
 				You_Win();
 				Menu(window);
+				pull_mas(Masiv);
+				Fox = 8;
+				Hod = 0;
 			}
 		}
 	}
@@ -539,7 +530,7 @@ int Menu(RenderWindow &window)
 
 
 	Music mus;
-	mus.openFromFile("music/track2.wav");
+	mus.openFromFile("music/track1.wav");
 	mus.play();
 
 
@@ -629,22 +620,27 @@ int Menu(RenderWindow &window)
 			}
 		}
 
+		while(window.pollEvent(event))
+		{
+			//clear event buffer!
+		}
+
 		window.draw(background_sprite);
 
-		playerScoreString4.clear();
-		playerScoreString4.str("");
-		text.setPosition(1000, 100);
-		pixelPos = Mouse::getPosition(window);
-		playerScoreString4 << pixelPos.x;
-		text.setString("H: " + playerScoreString4.str());
-		window.draw(text);
+		// playerScoreString4.clear();
+		// playerScoreString4.str("");
+		// text.setPosition(1000, 100);
+		// pixelPos = Mouse::getPosition(window);
+		// playerScoreString4 << pixelPos.x;
+		// text.setString("H: " + playerScoreString4.str());
+		// window.draw(text);
 
-		playerScoreString4.clear();
-		playerScoreString4.str("");
-		text.setPosition(1000, 150);
-		playerScoreString4 << pixelPos.y;
-		text.setString("V: " + playerScoreString4.str());
-		window.draw(text);
+		// playerScoreString4.clear();
+		// playerScoreString4.str("");
+		// text.setPosition(1000, 150);
+		// playerScoreString4 << pixelPos.y;
+		// text.setString("V: " + playerScoreString4.str());
+		// window.draw(text);
 
 		window.draw(play_sprite);
 		window.draw(exit_sprite);
@@ -654,4 +650,33 @@ int Menu(RenderWindow &window)
 	}
 	
 	return k;
+}
+
+void pull_mas(int *Masiv)
+{
+	int radok, stovbec;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			Masiv[i * N + j] = 0;
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		radok = 0 + rand() % 8;
+		stovbec = 0 + rand() % 8;
+		if (Masiv[radok * N + stovbec] == 1)
+		{
+			i--;
+		}
+		else
+		{
+			Masiv[radok * N + stovbec] = 1;
+		}
+		
+		
+	}
 }
